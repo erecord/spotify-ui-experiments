d4a7c4d1b0df01a85e7f6067ce01812274267c8e
Elliott Record <elliottrecord@gmail.com>
2020-11-15 19:57:02 +0000

    Updated spotifyAuth.vue to await setAuthTokens method

--- frontend/pages/spotifyAuth.vue
+++ frontend/pages/spotifyAuth.vue
@@ -1,8 +1,4 @@
-<template>
-  <div />
-</template>
-
-<script>
+<script lang="ts">
 import {
   useContext,
   defineComponent,
@@ -13,24 +9,15 @@ import useSpotify from "../hooks/useSpotify";
 export default defineComponent({
   setup() {
     const { query, redirect } = useContext();
-    const code = query.value.code;
-
     const { setAuthTokens } = useSpotify();
 
-    onMounted(() => {
-      const resultSuccess = setAuthTokens(
-        code,
-        `${process.env.NUXT_ENV_FRONTEND_URL}/spotifyAuth`
-      ).then(() => {
-        console.log(code);
-      });
+    const code: string = query.value.code;
 
-      if (resultSuccess) {
-        redirect("/");
-      } else {
-        console.error("spotifyAuth failed to set auth tokens");
-      }
-    });
+    const resultSuccess = async () => await setAuthTokens(code);
+
+    resultSuccess()
+      .then(() => redirect("/"))
+      .catch(() => console.error("spotifyAuth failed to set auth tokens"));
   }
 });
 </script>
